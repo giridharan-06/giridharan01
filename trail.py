c0cd59219cfdd30a39edace1599eb3e4a5a87b60
Giridharan S <giridharansasikumar06@gmail.com>
2026-07-06 19:14:26 +0530

    python trail

---
+++ trail.py
@@ -0,0 +1,34 @@
+##import keyword
+  ## print(keyword.kwlist)
+
+l=[1,2,3,4,5,6,7]
+#l2=l.pop()
+#print(l2)
+print(len(l))
+print(l)
+print(l[3])
+print(l[-4])
+print(l[::])
+print(l[:])
+print(l[::-1])
+print(l[-1:1:])
+print(l[0::-3])
+
+
+
+print('#############')
+
+
+
+print(1,2,3,4,5,6,7,8,9)
+print(10,20,30,sep=' ')
+print(10,20,30,sep='*')
+print(10,20,30,4,50,end='#')
+print(100,200,300,400)
+print(1,2,3,4,)
+#print(l2)
+a =[1,22,3,4,5]
+b=[1,2.3,(10+5j),'ravi',True]
+#bool(a)
+print(a)
+print(b)
